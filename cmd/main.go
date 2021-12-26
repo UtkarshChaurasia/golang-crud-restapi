2c793be0a71a61e479afb37471ad5eafe7604412
UtkarshChaurasia <msdianuc07@gmail.com>
2021-12-26 23:21:07 +0530

    database created

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+/*
+CREATE TABLE IF NOT EXISTS "newsfeed" (
+	"ID"	INTEGER NOT NULL,
+	"content"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+*/
 
 func main() {
-	fmt.Println("HELLO")
+
+	// creates a database of specified driver and filename
+	db, _ := sql.Open("sqlite3", "./newsfeed.db")
+
+	// prepares sql statement
+	stmt, _ := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS "newsfeed" (
+		"ID"	INTEGER NOT NULL,
+		"content"	TEXT,
+		PRIMARY KEY("ID" AUTOINCREMENT)
+	);
+	`)
+
+	// executes sql statement
+	stmt.Exec()
 }
